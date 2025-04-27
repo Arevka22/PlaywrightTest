c9db79632ed1b6069b50745899c2d22993464fc3
Arevka22 <98781907+Arevka22@users.noreply.github.com>
2025-04-27 13:00:43 +0300

    updated version

--- tests/CallUsWidgetTest.spec.ts
+++ tests/CallUsWidgetTest.spec.ts
@@ -1,5 +1,6 @@
-import { test, expect, APIRequestContext, Page } from "@playwright/test";
-import { TestPage } from "../pages/TestPage";
+import { test, expect, APIRequestContext, Page } from '@playwright/test';
+import { TestPage } from '../pages/TestPage';
+
 
 test.describe('"Call Us" widget functionality check', () => {
   let testPage: TestPage;
@@ -13,18 +14,17 @@ test.describe('"Call Us" widget functionality check', () => {
     await expect(testPage.callUsButton).toBeVisible();
   });
 
-  test("Fill form with valid values check", async () => {
+  test('Fill form with valid values check', async () => {
     await testPage.callUsButtonIsVisible();
     await testPage.clickCallUsButton();
     await expect(testPage.requestPopup).toBeVisible();
     await expect(testPage.requestPopupTitle).toBeVisible();
 
-    await testPage.enterFirstName("John");
-    await testPage.enterLastName("Doe");
-    await testPage.enterCountryCode("MA (+212)");
-    await testPage.enterPhoneNumber("123456789015");
-    await expect(testPage.confirmationSignButton).toBeEnabled();
-
+    await testPage.enterFirstName('John');
+    await testPage.enterLastName('Doe');
+    await testPage.enterCountryCode('MA (+212)');
+    await testPage.enterPhoneNumber('123456789015');
+    await expect(testPage.confirmationSignButton).toBeEnabled(); 
     await testPage.clickConfirmationButton();
     await expect(testPage.confirmationMsg).toBeVisible();
 
@@ -33,35 +33,30 @@ test.describe('"Call Us" widget functionality check', () => {
     await expect(testPage.callUsButton).toBeVisible();
   });
 
-  test("UI page view check", async ({ page }) => {
+  test('UI page view check', async ({page}) => {
     await expect(testPage.callUsButton).toBeVisible();
-    await expect(page).toHaveScreenshot("example-page.png", {
+    await expect(page).toHaveScreenshot('example-page.png', {
       maxDiffPixelRatio: 0.1, // 10% difference allowed
     });
   });
+  
 });
 
 //*********** API test example ************/
-test.describe("API tests", () => {
-  test("API GET request check", async ({
-    request,
-  }: {
-    request: APIRequestContext;
-  }) => {
-    const response = await request.get(
-      "https://qa1.chat-api.intaker.xyz/api/v2/Chat/countries-v2?directLink=demoorg",
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
+test.describe('API tests', () => {
+
+  test('API GET request check', async ({ request }: { request: APIRequestContext }) => {
+    const response = await request.get('https://qa1.chat-api.intaker.xyz/api/v2/Chat/countries-v2?directLink=demoorg', {
+      headers: {
+        'Content-Type': 'application/json', 
       },
-    );
+    });
+    expect(response.ok(), 'Response was not OK').toBeTruthy();         
+    expect(response.status(), 'Unexpected response status').toBe(200);     
 
-    expect(response.ok(), "Response was not OK").toBeTruthy();
-    expect(response.status(), "Unexpected response status").toBe(200);
-
-    const responseJson = await response.json();
-    expect(Array.isArray(responseJson)).toBeTruthy();
-    expect(responseJson.length).toBe(250);
+    const responseJson = await response.json();   
+    expect(Array.isArray(responseJson)).toBeTruthy();   
+    expect(responseJson.length).toBe(250);   
   });
+
 });
