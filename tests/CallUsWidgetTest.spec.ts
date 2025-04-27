83e70234b3c06c131ae10309f8c217105d8eef64
Arevka22 <98781907+Arevka22@users.noreply.github.com>
2025-04-27 23:05:41 +0300

    corrections

--- tests/CallUsWidgetTest.spec.ts
+++ tests/CallUsWidgetTest.spec.ts
@@ -38,14 +38,8 @@ test('UI page view check', async ({ page }) => {
   });
 });
 
-test('API GET request check', async ({
-  request,
-}: {
-  request: APIRequestContext;
-}) => {
-  const response = await request.get(
-    'https://qa1.chat-api.intaker.xyz/api/v2/Chat/countries-v2?directLink=demoorg',
-    {
+test('API GET request check', async ({ request }: { request: APIRequestContext }) => {
+  const response = await request.get('https://qa1.chat-api.intaker.xyz/api/v2/Chat/countries-v2?directLink=demoorg', {
       headers: {
         'Content-Type': 'application/json',
       },
